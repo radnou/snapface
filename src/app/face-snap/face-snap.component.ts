68049cd69498e7cde805fe25852dc5609d229a3b
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-02-03 17:26:42 +0100

    🎨 style(face-snap.component.html): improve code readability by adding proper indentation ✨ feat(face-snap.component.html): add button to increase or decrease snap count 🔧 refactor(face-snap.component.ts): change imageUrl to use picsum photos for random images ✨ feat(face-snap.component.ts): add snapped and buttonText properties to handle snap button state ✨ feat(face-snap.component.ts): add OnAddSnap method to handle snap button click event

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,24 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
-    standalone: true,
+  standalone: true,
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.css'
 })
 export class FaceSnapComponent {
-  title!:string;
-  description!:string;
-  createdDate!:Date;
-  snaps!:number;
-imageUrl!: string;
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+  snapped: boolean;
+  buttonText: string;
 
-  constructor(){
+  constructor() {
     this.title = "test";
     this.description = " description";
     this.createdDate = new Date();
-    this.snaps=0;
-    this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snaps = 0;
+    this.imageUrl = "https://picsum.photos/600/600";
+    this.snapped = false;
+    this.buttonText = 'Oh Snap!';
+
+  }
+
+  OnAddSnap() {
+    if (this.buttonText === 'Oh Snap!') {
+      this.snaps++;
+      this.buttonText = 'Oops, unSnap!';
+    } else {
+      this.snaps--;
+      this.buttonText = 'Oh Snap!';
+    }
+
 
   }
 }
