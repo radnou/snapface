51f33cd20126b1e7ebf5e37074f757efc773cb0f
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-02-10 16:15:57 +0100

    🔧 chore: add ESLint configuration and scripts for linting 🔧 chore: add Angular ESLint dependencies to package.json 🔧 chore: add lint script to package.json 🔧 chore: add lint configuration to angular.json 🔧 chore: add ESLint schematic to angular.json 🔧 chore: add ESLint configuration file .eslintrc.json 🔧 chore: add id property to FaceSnap model 🔧 chore: refactor app.component.ts to use an array of FaceSnap objects 🔧 chore: refactor app.component.html to use *ngFor to iterate over FaceSnap array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,34 +12,39 @@ import { FaceSnap } from './model /face-snap.model';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  mySnap: FaceSnap;
-  myOtherSnap: FaceSnap;
-  myLastSnap: FaceSnap;
+  faceSnaps: FaceSnap[];
 
   constructor() {
-    this.mySnap = {
-      title: 'Archibald',
-      description: 'Mon meilleur ami depuis tout petit !',
-      imageUrl:
-        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-    };
-    this.myOtherSnap = {
-      title: 'Three Rock Mountain',
-      description: 'Un endroit magnifique pour les randonnées.',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-      location: 'France',
-    };
-    this.myLastSnap = {
-      title: 'Un bon repas',
-      description: "Mmmh que c'est bon !",
-      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-      createdDate: new Date(),
-      snaps: 0,
-    };
+    this.faceSnaps = [
+      {
+        id: 1,
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        imageUrl:
+          'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+        location: 'Paris',
+      },
+      {
+        id: 2,
+        title: 'Three Rock Mountain',
+        description: 'Un endroit magnifique pour les randonnées.',
+        imageUrl:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        createdDate: new Date(),
+        snaps: 6,
+        location: 'la montagne',
+      },
+      {
+        id: 3,
+        title: 'Un bon repas',
+        description: "Mmmh que c'est bon !",
+        imageUrl:
+          'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        createdDate: new Date(),
+        snaps: 0,
+      },
+    ];
   }
 }
