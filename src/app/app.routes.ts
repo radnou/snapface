fb638adfc0560fac6522cab7eaec96d2024684a4
Radnoumane MOSSABELY <mossabelyradnoumane@gmail.com>
2024-03-03 04:05:47 +0100

    feat(RouterLink): manager route - Update Route Component - nav in Header - NavigateByUrl by injecting router in landingPageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,14 @@ import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
-  { path: 'facesnaps', component: FaceSnapListComponent },
-  { path: '', component: LandingPageComponent },
+  {
+    path: 'facesnaps',
+    component: FaceSnapListComponent,
+    data: { animation: 'enterLeavePage' },
+  },
+  {
+    path: '',
+    component: LandingPageComponent,
+    data: { animation: 'openClosePage' },
+  },
 ];
